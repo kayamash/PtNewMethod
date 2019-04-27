ea46ab5653f01bb26ed62bd22a76bdd43f7a5415
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-04-28 02:43:27 +0900

    Add New Method

--- run.cpp
+++ run.cpp
@@ -25,7 +25,6 @@ void run(){
   }
 
   if(!chain1)cout<<"tree failed!"<<endl;
-  cout<<"chain->Add() end"<<endl;
   cout<<"Total Events are "<<chain1->GetEntries()<<endl;
   PtNewMethod m(chain1,trigger,proc);
   const Int_t events = (EventFullScan) ? (chain1->GetEntries()) : (1000000);
