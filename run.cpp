f357c19e88213a2925e066bb14a820c8e7876b8b
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-04-27 22:28:19 +0900

    Add New Method

--- run.cpp
+++ run.cpp
@@ -1,4 +1,9 @@
-#include "PtNewMethod.C"
+#include <vector>
+#include <TGraphErrors.h>
+#include <TLegend.h>
+#include <TMath.h>
+#include <TBranch.h>
+#include "PtNewMethod.cpp"
 
 const string inputfilelist = "/home/kayamash/efflist/20190416data18_physics_Main_Ztap.list";
 const string outputfilename = "/gpfs/fs6001/kayamash/Mywork/efficiencyloopoutput/LUT/data18_physics_Main_ZtapMU4.root";
@@ -9,7 +14,6 @@ const Int_t proc = 1;//Jpsitap = 1,Ztap = 3
 const bool EventFullScan = kTRUE;//kTRUE = All Event,kFALSE = 1000000 events test
 
 void run(){
-  gROOT->LoadMacro("PtNewMethod.C");
   //Add chain
   TChain *chain = new TChain("t_tap");
   std::ifstream ifs(inputfilelist.c_str());
@@ -24,5 +28,7 @@ void run(){
 
   TFile *output_file = new TFile(outputfilename.c_str(),"RECREATE");
   m.Finalize(output_file,LUTname);
+  delete  output_file;
+  delete chain;
 }
 
