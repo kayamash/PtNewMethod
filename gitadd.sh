b3a8236ca232b8c6167ed4eea9b4e6b08f04bccc
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-04-27 15:29:45 +0900

    Add New Method

---
+++ gitadd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+branch="master"
+file1="run.cpp"
+file2="PtNewMethod.cxx"
+file3="PtNewMethod.chh"
+file4="gitadd.sh"
+add="git add "
+message="Add New Method"
+push="git push origin "
+
+eval $add$file1
+eval $add$file2
+eval $add$file3
+eval $add$file4
+#git add -A
+git commit -m "${message}"
+eval $push$branch
+
+
+
+
