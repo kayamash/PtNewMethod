e3b7ef9c127328ebeca27491b4715037b470891a
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-04-30 23:59:27 +0900

    Add New Method

--- kayamashForLUT.cpp
+++ kayamashForLUT.cpp
@@ -162,12 +162,11 @@ bool kayamashForLUT::WriteLUT(TProfile *prof,Int_t par1,Int_t par2,Int_t par3,In
 	Double_t chi = 0;
 	if(prof->GetEntries() != 0){
 		TF1 *fitProf = new TF1("fitProf","[0]*x + [1]*x*x",0.,0.3);
-		fitProf->FixParameter(0,0.);
-		fitProf->SetParameter(1,fOrder);
-		fitProf->SetParameter(2,sOrder);
+		fitProf->SetParameter(0,fOrder);
+		fitProf->SetParameter(1,sOrder);
 		prof->Fit(fitProf,"I","",0.,0.3);
-		p0 = fitProf->GetParameter(1);
-		p1 = fitProf->GetParameter(2);
+		p0 = fitProf->GetParameter(0);
+		p1 = fitProf->GetParameter(1);
 		chi = fitProf->GetChisquare();
 	}
 
