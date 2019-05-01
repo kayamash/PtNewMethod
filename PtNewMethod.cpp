108059efc8f08a30bcf138a6610dc409d18640dd
kayamash <yamashita@stu.kobe-u.ac.jp>
2019-05-01 17:22:47 +0900

    Add New Method

--- PtNewMethod.cpp
+++ PtNewMethod.cpp
@@ -31,7 +31,7 @@ const Int_t eta_max = 16;
 const Int_t phi_max = 15;
 
 bool PtNewMethod::CutAll(Int_t Tagpass,Int_t L1pass){
-	if(Tagpass > -1 && m_tag_proc == m_proc){
+	if(Tagpass > -1 && L1pass > -1 && m_tag_proc == m_proc){
     //if(m_sumReqdRL1 < m_tp_extdR && 0.2 < m_tp_extdR && m_sumReqdREF < m_tp_dR && Tagpass > -1 && m_tag_proc == m_proc && L1pass > -1 && m_poff_charge*m_tag_charge == -1){
 		return kTRUE;
 	}else{
